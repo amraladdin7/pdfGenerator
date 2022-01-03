23a7aa59082eace97f5a2543f7e1c9dfc52870db
Rawan Elkhouly <rawanahmad977@gmail.com>
2022-01-03 02:10:34 +0200

    Update appContract.js

--- force-app/main/default/lwc/appContract/appContract.js
+++ force-app/main/default/lwc/appContract/appContract.js
@@ -1,15 +1,9 @@
 import { LightningElement } from 'lwc';
 
 export default class AppContract extends LightningElement {
-    language;
     src;
     text;
     description;
-
-    handleLanguageChange(event) {
-        this.language = event.detail.language;
-        console.log(this.language);
-    }
 
     handleTextChange(event) {
         this.text = event.detail.text;
