319bfb49175ed074edeec03c4a5695ecddb0ba84
Rawan Elkhouly <rawanahmad977@gmail.com>
2022-01-11 17:05:48 +0200

    Update inputText.js

--- force-app/main/default/lwc/inputText/inputText.js
+++ force-app/main/default/lwc/inputText/inputText.js
@@ -5,7 +5,6 @@ export default class InputText extends LightningElement {
 
     handleChange(event) {
         this.text = event.target.value;
-        console.log(this.text);
         this.dispatchEvent(new CustomEvent('textchange', {detail: {text:this.text}}));
     }
 }
