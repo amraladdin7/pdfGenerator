575e1014c20e6aaacfe52ff46b6f315beb131e4c
Rawan Elkhouly <rawanahmad977@gmail.com>
2022-01-11 17:05:53 +0200

    Create listViewProducts.js

---
+++ force-app/main/default/lwc/listViewProducts/listViewProducts.js
@@ -0,0 +1,67 @@
+import { LightningElement, track, api, wire } from 'lwc';
+import ProductsToGet from '@salesforce/apex/ProductsGetter.ProductsToGet';
+
+export default class ListViewProducts extends LightningElement {
+
+    @track isLoading = false;
+    text;
+    Products;
+
+    handleTextChange(event) {
+        this.text = event.detail.text;
+    }
+
+    @wire(ProductsToGet, {})
+    wiredPdf(result){
+        if(result.data){
+            this.Products = result.data;
+            console.log(this.Products);
+        }
+        else {
+            console.log(result.error)
+        }
+    }
+
+    ISBNs = [];
+    Quantities = [];
+    ProductsData = [];
+
+    search(){
+        try{
+            console.log("search")
+            //Getting ISBNs & Quantities
+            let tmp = "";
+            console.log(this.text.length)
+            for(let i = 0; i<this.text.length; i++){
+                if(this.text[i] != " "){
+                    tmp += this.text[i];
+                    console.log(tmp);
+                }
+                else if(tmp.length == 13){
+                    this.ISBNs.push(tmp);
+                    console.log(tmp);
+                    tmp = "";
+                }
+                else if(this.text[i] == " " ){
+                    this.Quantities.push(tmp);
+                    console.log(tmp)
+                    tmp = "";
+                }
+            }
+            console.log(this.ISBNs + " " + this.Quantities + "quantities");
+            //Getting the Products with matching ISBNs
+            let ISBNCounter = 0;
+            for(let i = 0; i<this.Products.length; i++){
+                if(this.Products[i].ProductCode == this.ISBNs[ISBNCounter]){
+                    console.log(this.Products[i].ProductCode == this.ISBNs[ISBNCounter]);
+                    this.ProductsData.push(this.Products[i]);
+                    ISBNCounter ++;
+                }
+            }
+            console.log(this.ProductsData);
+    }
+    catch(e){
+        console.log(e);
+    }
+    }
+}
